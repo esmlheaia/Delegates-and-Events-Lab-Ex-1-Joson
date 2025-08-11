b73f7bce97e943bc6cc886b0185b5c7d75c29b2d
esmlheaia <miya.solenee@gmail.com>
2025-08-11 19:48:35 +0800

    UI Created

--- AccountRegistration/Form1.Designer.cs
+++ AccountRegistration/Form1.Designer.cs
@@ -45,7 +45,7 @@
             this.textBox7 = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Next = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -170,7 +170,7 @@
             this.textBox7.MaximumSize = new System.Drawing.Size(400, 176);
             this.textBox7.MinimumSize = new System.Drawing.Size(50, 100);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(370, 100);
+            this.textBox7.Size = new System.Drawing.Size(400, 100);
             this.textBox7.TabIndex = 14;
             // 
             // comboBox1
@@ -181,21 +181,21 @@
             this.comboBox1.Size = new System.Drawing.Size(152, 21);
             this.comboBox1.TabIndex = 15;
             // 
-            // button1
+            // Next
             // 
-            this.button1.Location = new System.Drawing.Point(179, 268);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 16;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Next.Location = new System.Drawing.Point(158, 260);
+            this.Next.Name = "Next";
+            this.Next.Size = new System.Drawing.Size(75, 23);
+            this.Next.TabIndex = 16;
+            this.Next.Text = "Next";
+            this.Next.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(558, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(498, 450);
+            this.Controls.Add(this.Next);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.textBox7);
             this.Controls.Add(this.textBox6);
@@ -238,7 +238,7 @@
         private System.Windows.Forms.TextBox textBox7;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Next;
     }
 }
 
